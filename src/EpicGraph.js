45d351549ca8bcb2f95c4de9904d8166d46d2c6d
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2018-04-03 22:39:58 +1000

    Add graph rendering using echarts-for-react
    echarts-for-react provides a React component for echarts, but requires
    the user to install echarts and prop-types yourself for some reason.

--- src/EpicGraph.js
+++ src/EpicGraph.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
 import './EpicGraph.css';
 
 class EpicGraph extends Component {
@@ -12,9 +13,46 @@ class EpicGraph extends Component {
     return (
       <div className="EpicGraph">
         <p>Rendering Epic {epic.id}</p>
+        <ReactEcharts option={this.getOption()} />
         <ul style={{ display: 'none' }}>{resultList}</ul>
       </div>
     );
+  }
+
+  getOption() {
+    const results = this.props.results[this.props.selected];
+
+    const nearestInteger = results.map(x => Math.round(x));
+    const counts = this.countHits(nearestInteger);
+
+    // echarts datasets row-based key-value format
+    // https://ecomfe.github.io/echarts-doc/public/en/option.html#dataset.source
+
+    const dataset = counts.map(([length, count]) => ({
+      length: length,
+      count: count,
+    }));
+
+    return {
+      legend: {},
+      tooltip: {},
+      dataset: {
+        source: dataset,
+        dimensions: ['length', 'count'],
+      },
+      xAxis: { type: 'value' },
+      yAxis: {},
+      series: { type: 'bar' },
+    };
+  }
+
+  countHits(values) {
+    // https://stackoverflow.com/a/35101824/166389
+    // Although returns the list of key/value pairs, not a Map
+    return [...new Set(values)].map(x => [
+      x,
+      values.filter(y => y === x).length,
+    ]);
   }
 }
 
